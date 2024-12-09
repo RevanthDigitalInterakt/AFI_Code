2edc9e268823d5755303594dfa901df77748bfa7
RevanthDigitalInterakt <revanth@rezultize.com>
2024-12-09 10:39:45 +0530

    aws final code v2

--- sourcecode/main.py
+++ sourcecode/main.py
@@ -14,7 +14,6 @@ async def root():
 app.include_router(leads.router)
 app.include_router(Accounts.router, prefix="/accounts", tags=["accounts"])
 app.include_router(contacts.router, prefix="/contacts", tags=["contacts"])
-# app.include_router(contacts.router)
-# app.include_router(accounts.router)
+
 
 
